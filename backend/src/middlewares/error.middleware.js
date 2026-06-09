279a4b03f53c7a50f360241b06de92538b2cd343
ThyagoRafael <thyagorafael57@gmail.com>
2026-06-09 14:39:21 -0300

    feat: adicionar middleware de erro global e classe para erros de negócio

---
+++ backend/src/middlewares/error.middleware.js
@@ -0,0 +1,21 @@
+import { ZodError } from "zod";
+import { AppError } from "../errors/AppError.js";
+
+export function errorMiddleware(err, req, res, next) {
+	if (err instanceof AppError) {
+		res.status(err.statusCode).json({ message: err.message });
+		return;
+	}
+
+	if (err instanceof ZodError) {
+		const firstError = err.issues[0];
+
+		return res.status(400).json({
+			field: firstError.path[0],
+			message: firstError.message,
+		});
+	}
+
+	console.error(err);
+	res.status(500).json({ message: "Erro inesperado no servidor" });
+}
