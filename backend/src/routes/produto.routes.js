244ed9462b8b1704ee0649efd389f1a376cf28b2
ThyagoRafael <thyagorafael57@gmail.com>
2026-06-22 15:57:45 -0300

    feat: adicionar middleware de autenticação nas rotas POST e PATCH de produtos

--- backend/src/routes/produto.routes.js
+++ backend/src/routes/produto.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import produtoController from "../controllers/produto.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const produtoRoutes = Router();
 
-produtoRoutes.post("/", produtoController.create);
+produtoRoutes.post("/", authMiddleware, produtoController.create);
 produtoRoutes.get("/", produtoController.list);
-produtoRoutes.patch("/:produtoId", produtoController.update);
+produtoRoutes.patch("/:produtoId", authMiddleware, produtoController.update);
 
 export { produtoRoutes };
