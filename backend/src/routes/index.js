bc75101037608a8403b9deb4da230636806872bb
ThyagoRafael <thyagorafael57@gmail.com>
2026-06-22 15:56:35 -0300

    feat: adicionar rotas e controller do dashboard

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,6 +4,9 @@ import { categoriaRoutes } from "./categoria.routes.js";
 import { produtoRoutes } from "./produto.routes.js";
 import { voucherRoutes } from "./voucher.routes.js";
 import { vendedorRoutes } from "./vendedor.routes.js";
+import { vendaRoutes } from "./venda.routes.js";
+import { movimentacaoEstoqueRoutes } from "./movimentacao_estoque.routes.js";
+import { dashboardRoutes } from "./dashboard.routes.js";
 
 const routes = Router();
 
@@ -12,5 +15,8 @@ routes.use("/categorias", categoriaRoutes);
 routes.use("/produtos", produtoRoutes);
 routes.use("/vouchers", voucherRoutes);
 routes.use("/vendedores", vendedorRoutes);
+routes.use("/vendas", vendaRoutes);
+routes.use("/movimentacoes", movimentacaoEstoqueRoutes);
+routes.use("/dashboard", dashboardRoutes);
 
 export { routes };
