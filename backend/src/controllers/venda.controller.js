fd5e6a0d15addfaf3b0a823acced5dd8ea268a83
ThyagoRafael <thyagorafael57@gmail.com>
2026-06-22 15:53:39 -0300

    feat: adicionar rotas, controllers e validações de vendas

---
+++ backend/src/controllers/venda.controller.js
@@ -0,0 +1,41 @@
+import { prisma } from "../config/prisma.js";
+
+class VendaController {
+	async list(req, res) {
+		const vendas = await prisma.venda.findMany({
+			select: {
+				numero_pedido: true,
+				criado_em: true,
+				atualizado_em: true,
+
+				venda_item: {
+					select: {
+						produto: {
+							select: {
+								nome_produto: true,
+							},
+						},
+					},
+				},
+				cliente: {
+					select: {
+						usuario: {
+							select: {
+								nome_completo_usuario: true,
+							},
+						},
+					},
+				},
+				vendedor: {
+					select: {
+						nome_completo_vendedor: true,
+					},
+				},
+			},
+		});
+
+		res.status(200).json(vendas);
+	}
+}
+
+export default new VendaController();
