04ae11a74d9592a13ab80edb342ca88b0bdabf2e
isaacnwc <isaac.canario@outlook.com>
2026-06-24 23:03:46 -0300

    mudanca no routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,12 +4,9 @@ import { categoriaRoutes } from "./categoria.routes.js";
 import { produtoRoutes } from "./produto.routes.js";
 import { voucherRoutes } from "./voucher.routes.js";
 import { vendedorRoutes } from "./vendedor.routes.js";
-<<<<<<< HEAD
-=======
 import { vendaRoutes } from "./venda.routes.js";
 import { movimentacaoEstoqueRoutes } from "./movimentacao_estoque.routes.js";
 import { dashboardRoutes } from "./dashboard.routes.js";
->>>>>>> e43cb56 (adicionando backend da main)
 
 const routes = Router();
 
@@ -18,11 +15,8 @@ routes.use("/categorias", categoriaRoutes);
 routes.use("/produtos", produtoRoutes);
 routes.use("/vouchers", voucherRoutes);
 routes.use("/vendedores", vendedorRoutes);
-<<<<<<< HEAD
-=======
 routes.use("/vendas", vendaRoutes);
 routes.use("/movimentacoes", movimentacaoEstoqueRoutes);
 routes.use("/dashboard", dashboardRoutes);
->>>>>>> e43cb56 (adicionando backend da main)
 
 export { routes };
