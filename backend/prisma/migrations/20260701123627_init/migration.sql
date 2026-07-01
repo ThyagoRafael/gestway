76914a9de6600e72594c3a9d0abf77096f119413
isaacnwc <isaac.canario@outlook.com>
2026-07-01 17:57:22 -0300

    nao sei

---
+++ backend/prisma/migrations/20260701123627_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - Made the column `estoque_atual_produto` on table `produto` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE `produto` MODIFY `estoque_atual_produto` INTEGER NOT NULL;
