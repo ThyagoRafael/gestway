a8d279eea488dc5b9322fdd9ef0eb2925976e595
isaacnwc <isaac.canario@outlook.com>
2026-07-01 05:10:26 -0300

    mapeamento requisicao do carrinho

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,24 +1,26 @@
 import { Router } from "express";
-import { authRoutes } from "./auth.routes.js";
-import { categoriaRoutes } from "./categoria.routes.js";
-import { produtoRoutes } from "./produto.routes.js";
-import { voucherRoutes } from "./voucher.routes.js";
-import { vendedorRoutes } from "./vendedor.routes.js";
-import { vendaRoutes } from "./venda.routes.js";
+import { authRoutes }               from "./auth.routes.js";
+import { categoriaRoutes }          from "./categoria.routes.js";
+import { produtoRoutes }            from "./produto.routes.js";
+import { voucherRoutes }            from "./voucher.routes.js";
+import { vendedorRoutes }           from "./vendedor.routes.js";
+import { vendaRoutes }              from "./venda.routes.js";
 import { movimentacaoEstoqueRoutes } from "./movimentacao_estoque.routes.js";
-import { dashboardRoutes } from "./dashboard.routes.js";
-import { relatorioRoutes } from "./relatorio.routes.js";
+import { dashboardRoutes }          from "./dashboard.routes.js";
+import { relatorioRoutes }          from "./relatorio.routes.js";
+import { carrinhoRoutes }           from "./carrinho.routes.js";
 
 const routes = Router();
 
-routes.use("/auth", authRoutes);
-routes.use("/categorias", categoriaRoutes);
-routes.use("/produtos", produtoRoutes);
-routes.use("/vouchers", voucherRoutes);
-routes.use("/vendedores", vendedorRoutes);
-routes.use("/vendas", vendaRoutes);
+routes.use("/auth",          authRoutes);
+routes.use("/categorias",    categoriaRoutes);
+routes.use("/produtos",      produtoRoutes);
+routes.use("/vouchers",      voucherRoutes);
+routes.use("/vendedores",    vendedorRoutes);
+routes.use("/vendas",        vendaRoutes);
 routes.use("/movimentacoes", movimentacaoEstoqueRoutes);
-routes.use("/dashboard", dashboardRoutes);
-routes.use("/relatorios", relatorioRoutes);
+routes.use("/dashboard",     dashboardRoutes);
+routes.use("/relatorios",    relatorioRoutes);
+routes.use("/carrinho",      carrinhoRoutes);
 
 export { routes };
